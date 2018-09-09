1e945c20511d8c8053af5cf80a3f0ebb993f3753
Cameron Ross <elpenguino@gmail.com>
2018-09-09 04:20:19 -0300

    split bank DE data bins

--- src/bank1e.asm
+++ src/bank1e.asm
@@ -1,4 +1,11 @@
 .SEGMENT "BANK1E"
 .ORG $DE0000
 
-.INCBIN "bin/bank1e.bin"
+MAP_DATA_TILE_GRAPHICS_BLOCK_4: ;$DE0000
+	.INCBIN "src/bin/maptile_gfx/4.bin"
+
+MAP_DATA_ANIMATED_TILE_GFX_BLOCK_1: ;$DEF0E7
+	.INCBIN "src/bin/maptile_anim_gfx/1.bin"
+
+AUDIO_PACK_143: ;$DEFCDD
+	.INCBIN "src/bin/audiopacks/143.bin"
