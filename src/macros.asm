30831503771437e93bc84008865c586ac93839e2
Cameron Ross <elpenguino@gmail.com>
2018-09-08 22:32:06 -0300

    use a DISPLAY_TEXT_PTR macro for text pointers

--- src/macros.asm
+++ src/macros.asm
@@ -6,3 +6,11 @@
 	.BYTE .LOBYTE(.HIWORD(addr))
 	.WORD .LOWORD(addr)
 .endmacro
+
+.macro DISPLAY_TEXT_PTR addr
+	LDA #.LOWORD(addr)
+	STA $0E
+	LDA #.HIWORD(addr)
+	STA $10
+	JSL DISPLAY_TEXT
+.endmacro
