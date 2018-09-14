ec25c7261e0ca477b72bb8a27e5afad99488b89e
Cameron Ross <elpenguino@gmail.com>
2018-09-14 00:01:56 -0300

    split battle bgs, battle sprites

--- src/bank0b.asm
+++ src/bank0b.asm
@@ -1,8 +1,563 @@
 .SEGMENT "BANK0B"
 .ORG $CB0000
 
-BATTLEBGS_SECONDARY_DATA: ;$CB0000
-	.INCBIN "bin/unknowns/CB0000.bin"
+BATTLE_BACKGROUND_ARR_43: ;$CB0000
+	.INCBIN"bin/battle_bgs/arrangements/43.bin"
+
+BATTLE_BACKGROUND_ARR_60: ;$CB05F2
+	.INCBIN"bin/battle_bgs/arrangements/60.bin"
+
+BATTLE_BACKGROUND_ARR_41: ;$CB0B95
+	.INCBIN"bin/battle_bgs/arrangements/41.bin"
+
+BATTLE_BACKGROUND_ARR_62: ;$CB1121
+	.INCBIN"bin/battle_bgs/arrangements/62.bin"
+
+BATTLE_BACKGROUND_GFX_86: ;$CB16A5
+	.INCBIN"bin/battle_bgs/graphics/86.bin"
+
+BATTLE_BACKGROUND_ARR_61: ;$CB1C10
+	.INCBIN"bin/battle_bgs/arrangements/61.bin"
+
+BATTLE_BACKGROUND_ARR_63: ;$CB209F
+	.INCBIN"bin/battle_bgs/arrangements/63.bin"
+
+BATTLE_BACKGROUND_ARR_10: ;$CB2508
+	.INCBIN"bin/battle_bgs/arrangements/10.bin"
+
+BATTLE_BACKGROUND_ARR_40: ;$CB2958
+	.INCBIN"bin/battle_bgs/arrangements/40.bin"
+
+BATTLE_BACKGROUND_ARR_16: ;$CB2D84
+	.INCBIN"bin/battle_bgs/arrangements/16.bin"
+
+BATTLE_BACKGROUND_ARR_9: ;$CB31A3
+	.INCBIN"bin/battle_bgs/arrangements/9.bin"
+
+BATTLE_BACKGROUND_ARR_97: ;$CB353F
+	.INCBIN"bin/battle_bgs/arrangements/97.bin"
+
+BATTLE_BACKGROUND_ARR_26: ;$CB38CC
+	.INCBIN"bin/battle_bgs/arrangements/26.bin"
+
+BATTLE_BACKGROUND_ARR_66: ;$CB3C59
+	.INCBIN"bin/battle_bgs/arrangements/66.bin"
+
+BATTLE_BACKGROUND_ARR_6: ;$CB3FC1
+	.INCBIN"bin/battle_bgs/arrangements/6.bin"
+
+BATTLE_BACKGROUND_GFX_84: ;$CB431F
+	.INCBIN"bin/battle_bgs/graphics/84.bin"
+
+BATTLE_BACKGROUND_ARR_98: ;$CB4666
+	.INCBIN"bin/battle_bgs/arrangements/98.bin"
+
+BATTLE_BACKGROUND_ARR_4: ;$CB498F
+	.INCBIN"bin/battle_bgs/arrangements/4.bin"
+
+BATTLE_BACKGROUND_ARR_7: ;$CB4CB3
+	.INCBIN"bin/battle_bgs/arrangements/7.bin"
+
+BATTLE_BACKGROUND_GFX_9: ;$CB4FC7
+	.INCBIN"bin/battle_bgs/graphics/9.bin"
+
+BATTLE_BACKGROUND_ARR_8: ;$CB529E
+	.INCBIN"bin/battle_bgs/arrangements/8.bin"
+
+BATTLE_BACKGROUND_ARR_2: ;$CB556C
+	.INCBIN"bin/battle_bgs/arrangements/2.bin"
+
+BATTLE_BACKGROUND_ARR_84: ;$CB5828
+	.INCBIN"bin/battle_bgs/arrangements/84.bin"
+
+BATTLE_BACKGROUND_ARR_86: ;$CB5AA8
+	.INCBIN"bin/battle_bgs/arrangements/86.bin"
+
+BATTLE_BACKGROUND_ARR_51: ;$CB5D27
+	.INCBIN"bin/battle_bgs/arrangements/51.bin"
+
+BATTLE_BACKGROUND_ARR_64: ;$CB5F91
+	.INCBIN"bin/battle_bgs/arrangements/64.bin"
+
+BATTLE_BACKGROUND_ARR_53: ;$CB61D2
+	.INCBIN"bin/battle_bgs/arrangements/53.bin"
+
+BATTLE_BACKGROUND_ARR_5: ;$CB6411
+	.INCBIN"bin/battle_bgs/arrangements/5.bin"
+
+BATTLE_BACKGROUND_ARR_56: ;$CB662B
+	.INCBIN"bin/battle_bgs/arrangements/56.bin"
+
+BATTLE_BACKGROUND_ARR_93: ;$CB683F
+	.INCBIN"bin/battle_bgs/arrangements/93.bin"
+
+BATTLE_BACKGROUND_ARR_59: ;$CB6A3D
+	.INCBIN"bin/battle_bgs/arrangements/59.bin"
+
+BATTLE_BACKGROUND_ARR_89: ;$CB6C2B
+	.INCBIN"bin/battle_bgs/arrangements/89.bin"
+
+BATTLE_BACKGROUND_GFX_18: ;$CB6E17
+	.INCBIN"bin/battle_bgs/graphics/18.bin"
+
+BATTLE_BACKGROUND_ARR_52: ;$CB6FFF
+	.INCBIN"bin/battle_bgs/arrangements/52.bin"
+
+BATTLE_BACKGROUND_GFX_4: ;$CB71E5
+	.INCBIN"bin/battle_bgs/graphics/4.bin"
+
+BATTLE_BACKGROUND_GFX_25: ;$CB73CB
+	.INCBIN"bin/battle_bgs/graphics/25.bin"
+
+BATTLE_BACKGROUND_GFX_8: ;$CB75AF
+	.INCBIN"bin/battle_bgs/graphics/8.bin"
+
+BATTLE_BACKGROUND_ARR_95: ;$CB7781
+	.INCBIN"bin/battle_bgs/arrangements/95.bin"
+
+BATTLE_BACKGROUND_ARR_94: ;$CB794F
+	.INCBIN"bin/battle_bgs/arrangements/94.bin"
+
+BATTLE_BACKGROUND_ARR_101: ;$CB7B12
+	.INCBIN"bin/battle_bgs/arrangements/101.bin"
+
+BATTLE_BACKGROUND_ARR_36: ;$CB7CC9
+	.INCBIN"bin/battle_bgs/arrangements/36.bin"
+
+BATTLE_BACKGROUND_GFX_16: ;$CB7E7B
+	.INCBIN"bin/battle_bgs/graphics/16.bin"
+
+BATTLE_BACKGROUND_ARR_35: ;$CB8028
+	.INCBIN"bin/battle_bgs/arrangements/35.bin"
+
+BATTLE_BACKGROUND_GFX_53: ;$CB81CC
+	.INCBIN"bin/battle_bgs/graphics/53.bin"
+
+BATTLE_BACKGROUND_ARR_82: ;$CB836A
+	.INCBIN"bin/battle_bgs/arrangements/82.bin"
+
+BATTLE_BACKGROUND_ARR_12: ;$CB8501
+	.INCBIN"bin/battle_bgs/arrangements/12.bin"
+
+BATTLE_BACKGROUND_ARR_49: ;$CB868C
+	.INCBIN"bin/battle_bgs/arrangements/49.bin"
+
+BATTLE_BACKGROUND_ARR_85: ;$CB8816
+	.INCBIN"bin/battle_bgs/arrangements/85.bin"
+
+BATTLE_BACKGROUND_ARR_1: ;$CB899D
+	.INCBIN"bin/battle_bgs/arrangements/1.bin"
+
+BATTLE_BACKGROUND_ARR_99: ;$CB8B24
+	.INCBIN"bin/battle_bgs/arrangements/99.bin"
+
+BATTLE_BACKGROUND_ARR_96: ;$CB8CA8
+	.INCBIN"bin/battle_bgs/arrangements/96.bin"
+
+BATTLE_BACKGROUND_ARR_78: ;$CB8E2A
+	.INCBIN"bin/battle_bgs/arrangements/78.bin"
+
+BATTLE_BACKGROUND_GFX_15: ;$CB8FAB
+	.INCBIN"bin/battle_bgs/graphics/15.bin"
+
+BATTLE_BACKGROUND_GFX_10: ;$CB9124
+	.INCBIN"bin/battle_bgs/graphics/10.bin"
+
+BATTLE_BACKGROUND_GFX_82: ;$CB929A
+	.INCBIN"bin/battle_bgs/graphics/82.bin"
+
+BATTLE_BACKGROUND_GFX_47: ;$CB9409
+	.INCBIN"bin/battle_bgs/graphics/47.bin"
+
+BATTLE_BACKGROUND_ARR_34: ;$CB955D
+	.INCBIN"bin/battle_bgs/arrangements/34.bin"
+
+BATTLE_BACKGROUND_ARR_44: ;$CB96AC
+	.INCBIN"bin/battle_bgs/arrangements/44.bin"
+
+BATTLE_BACKGROUND_ARR_102: ;$CB97F0
+	.INCBIN"bin/battle_bgs/arrangements/102.bin"
+
+BATTLE_BACKGROUND_GFX_52: ;$CB9932
+	.INCBIN"bin/battle_bgs/graphics/52.bin"
+
+BATTLE_BACKGROUND_ARR_50: ;$CB9A71
+	.INCBIN"bin/battle_bgs/arrangements/50.bin"
+
+BATTLE_BACKGROUND_GFX_27: ;$CB9BAB
+	.INCBIN"bin/battle_bgs/graphics/27.bin"
+
+BATTLE_BACKGROUND_GFX_98: ;$CB9CC8
+	.INCBIN"bin/battle_bgs/graphics/98.bin"
+
+BATTLE_BACKGROUND_GFX_5: ;$CB9DE3
+	.INCBIN"bin/battle_bgs/graphics/5.bin"
+
+BATTLE_BACKGROUND_GFX_3: ;$CB9EF3
+	.INCBIN"bin/battle_bgs/graphics/3.bin"
+
+BATTLE_BACKGROUND_GFX_6: ;$CB9FFA
+	.INCBIN"bin/battle_bgs/graphics/6.bin"
+
+BATTLE_BACKGROUND_GFX_17: ;$CBA100
+	.INCBIN"bin/battle_bgs/graphics/17.bin"
+
+BATTLE_BACKGROUND_ARR_15: ;$CBA200
+	.INCBIN"bin/battle_bgs/arrangements/15.bin"
+
+BATTLE_BACKGROUND_ARR_68: ;$CBA2FF
+	.INCBIN"bin/battle_bgs/arrangements/68.bin"
+
+BATTLE_BACKGROUND_ARR_28: ;$CBA3FA
+	.INCBIN"bin/battle_bgs/arrangements/28.bin"
+
+BATTLE_BACKGROUND_GFX_85: ;$CBA4EB
+	.INCBIN"bin/battle_bgs/graphics/85.bin"
+
+BATTLE_BACKGROUND_GFX_1: ;$CBA5D4
+	.INCBIN"bin/battle_bgs/graphics/1.bin"
+
+BATTLE_BACKGROUND_ARR_14: ;$CBA6BD
+	.INCBIN"bin/battle_bgs/arrangements/14.bin"
+
+BATTLE_BACKGROUND_GFX_89: ;$CBA7A5
+	.INCBIN"bin/battle_bgs/graphics/89.bin"
+
+BATTLE_BACKGROUND_GFX_93: ;$CBA889
+	.INCBIN"bin/battle_bgs/graphics/93.bin"
+
+BATTLE_BACKGROUND_ARR_31: ;$CBA96A
+	.INCBIN"bin/battle_bgs/arrangements/31.bin"
+
+BATTLE_BACKGROUND_GFX_68: ;$CBAA46
+	.INCBIN"bin/battle_bgs/graphics/68.bin"
+
+BATTLE_BACKGROUND_GFX_54: ;$CBAB1E
+	.INCBIN"bin/battle_bgs/graphics/54.bin"
+
+BATTLE_BACKGROUND_GFX_99: ;$CBABF5
+	.INCBIN"bin/battle_bgs/graphics/99.bin"
+
+BATTLE_BACKGROUND_GFX_24: ;$CBACCB
+	.INCBIN"bin/battle_bgs/graphics/24.bin"
+
+BATTLE_BACKGROUND_ARR_65: ;$CBAD9C
+	.INCBIN"bin/battle_bgs/arrangements/65.bin"
+
+BATTLE_BACKGROUND_ARR_47: ;$CBAE6C
+	.INCBIN"bin/battle_bgs/arrangements/47.bin"
+
+BATTLE_BACKGROUND_ARR_55: ;$CBAF2D
+	.INCBIN"bin/battle_bgs/arrangements/55.bin"
+
+BATTLE_BACKGROUND_ARR_27: ;$CBAFED
+	.INCBIN"bin/battle_bgs/arrangements/27.bin"
+
+BATTLE_BACKGROUND_GFX_101: ;$CBB0AD
+	.INCBIN"bin/battle_bgs/graphics/101.bin"
+
+BATTLE_BACKGROUND_GFX_2: ;$CBB16C
+	.INCBIN"bin/battle_bgs/graphics/2.bin"
+
+BATTLE_BACKGROUND_ARR_3: ;$CBB22A
+	.INCBIN"bin/battle_bgs/arrangements/3.bin"
+
+BATTLE_BACKGROUND_ARR_70: ;$CBB2E1
+	.INCBIN"bin/battle_bgs/arrangements/70.bin"
+
+BATTLE_BACKGROUND_ARR_45: ;$CBB393
+	.INCBIN"bin/battle_bgs/arrangements/45.bin"
+
+BATTLE_BACKGROUND_GFX_7: ;$CBB441
+	.INCBIN"bin/battle_bgs/graphics/7.bin"
+
+BATTLE_BACKGROUND_ARR_69: ;$CBB4ED
+	.INCBIN"bin/battle_bgs/arrangements/69.bin"
+
+BATTLE_BACKGROUND_ARR_54: ;$CBB58D
+	.INCBIN"bin/battle_bgs/arrangements/54.bin"
+
+BATTLE_BACKGROUND_ARR_13: ;$CBB62D
+	.INCBIN"bin/battle_bgs/arrangements/13.bin"
+
+BATTLE_BACKGROUND_GFX_70: ;$CBB6CD
+	.INCBIN"bin/battle_bgs/graphics/70.bin"
+
+BATTLE_BACKGROUND_GFX_91: ;$CBB767
+	.INCBIN"bin/battle_bgs/graphics/91.bin"
+
+BATTLE_BACKGROUND_GFX_14: ;$CBB800
+	.INCBIN"bin/battle_bgs/graphics/14.bin"
+
+BATTLE_BACKGROUND_GFX_36: ;$CBB88E
+	.INCBIN"bin/battle_bgs/graphics/36.bin"
+
+BATTLE_BACKGROUND_ARR_58: ;$CBB91A
+	.INCBIN"bin/battle_bgs/arrangements/58.bin"
+
+BATTLE_BACKGROUND_ARR_23: ;$CBB9A5
+	.INCBIN"bin/battle_bgs/arrangements/23.bin"
+
+BATTLE_BACKGROUND_GFX_20: ;$CBBA2F
+	.INCBIN"bin/battle_bgs/graphics/20.bin"
+
+BATTLE_BACKGROUND_ARR_18: ;$CBBAB7
+	.INCBIN"bin/battle_bgs/arrangements/18.bin"
+
+BATTLE_BACKGROUND_GFX_65: ;$CBBB3C
+	.INCBIN"bin/battle_bgs/graphics/65.bin"
+
+BATTLE_BACKGROUND_ARR_32: ;$CBBBC0
+	.INCBIN"bin/battle_bgs/arrangements/32.bin"
+
+BATTLE_BACKGROUND_ARR_24: ;$CBBC3D
+	.INCBIN"bin/battle_bgs/arrangements/24.bin"
+
+BATTLE_BACKGROUND_ARR_20: ;$CBBCB9
+	.INCBIN"bin/battle_bgs/arrangements/20.bin"
+
+BATTLE_BACKGROUND_ARR_30: ;$CBBD32
+	.INCBIN"bin/battle_bgs/arrangements/30.bin"
+
+BATTLE_BACKGROUND_GFX_102: ;$CBBDAA
+	.INCBIN"bin/battle_bgs/graphics/102.bin"
+
+BATTLE_BACKGROUND_ARR_88: ;$CBBE21
+	.INCBIN"bin/battle_bgs/arrangements/88.bin"
+
+BATTLE_BACKGROUND_ARR_57: ;$CBBE97
+	.INCBIN"bin/battle_bgs/arrangements/57.bin"
+
+BATTLE_BACKGROUND_ARR_48: ;$CBBF0A
+	.INCBIN"bin/battle_bgs/arrangements/48.bin"
+
+BATTLE_BACKGROUND_GFX_32: ;$CBBF7D
+	.INCBIN"bin/battle_bgs/graphics/32.bin"
+
+BATTLE_BACKGROUND_ARR_91: ;$CBBFEE
+	.INCBIN"bin/battle_bgs/arrangements/91.bin"
+
+BATTLE_BACKGROUND_GFX_51: ;$CBC05E
+	.INCBIN"bin/battle_bgs/graphics/51.bin"
+
+BATTLE_BACKGROUND_ARR_67: ;$CBC0CA
+	.INCBIN"bin/battle_bgs/arrangements/67.bin"
+
+BATTLE_BACKGROUND_ARR_29: ;$CBC133
+	.INCBIN"bin/battle_bgs/arrangements/29.bin"
+
+BATTLE_BACKGROUND_GFX_34: ;$CBC19A
+	.INCBIN"bin/battle_bgs/graphics/34.bin"
+
+BATTLE_BACKGROUND_ARR_25: ;$CBC200
+	.INCBIN"bin/battle_bgs/arrangements/25.bin"
+
+BATTLE_BACKGROUND_ARR_17: ;$CBC265
+	.INCBIN"bin/battle_bgs/arrangements/17.bin"
+
+BATTLE_BACKGROUND_GFX_100: ;$CBC2C9
+	.INCBIN"bin/battle_bgs/graphics/100.bin"
+
+BATTLE_BACKGROUND_GFX_23: ;$CBC32C
+	.INCBIN"bin/battle_bgs/graphics/23.bin"
+
+BATTLE_BACKGROUND_ARR_19: ;$CBC38F
+	.INCBIN"bin/battle_bgs/arrangements/19.bin"
+
+BATTLE_BACKGROUND_ARR_46: ;$CBC3F2
+	.INCBIN"bin/battle_bgs/arrangements/46.bin"
+
+BATTLE_BACKGROUND_ARR_33: ;$CBC453
+	.INCBIN"bin/battle_bgs/arrangements/33.bin"
+
+BATTLE_BACKGROUND_ARR_22: ;$CBC4B2
+	.INCBIN"bin/battle_bgs/arrangements/22.bin"
+
+BATTLE_BACKGROUND_ARR_21: ;$CBC50F
+	.INCBIN"bin/battle_bgs/arrangements/21.bin"
+
+BATTLE_BACKGROUND_ARR_11: ;$CBC568
+	.INCBIN"bin/battle_bgs/arrangements/11.bin"
+
+BATTLE_BACKGROUND_GFX_31: ;$CBC5C1
+	.INCBIN"bin/battle_bgs/graphics/31.bin"
+
+BATTLE_BACKGROUND_GFX_88: ;$CBC618
+	.INCBIN"bin/battle_bgs/graphics/88.bin"
+
+BATTLE_BACKGROUND_GFX_19: ;$CBC66E
+	.INCBIN"bin/battle_bgs/graphics/19.bin"
+
+BATTLE_BACKGROUND_GFX_21: ;$CBC6BD
+	.INCBIN"bin/battle_bgs/graphics/21.bin"
+
+BATTLE_BACKGROUND_ARR_92: ;$CBC70B
+	.INCBIN"bin/battle_bgs/arrangements/92.bin"
+
+BATTLE_BACKGROUND_GFX_59: ;$CBC753
+	.INCBIN"bin/battle_bgs/graphics/59.bin"
+
+BATTLE_BACKGROUND_GFX_22: ;$CBC79B
+	.INCBIN"bin/battle_bgs/graphics/22.bin"
+
+BATTLE_BACKGROUND_GFX_97: ;$CBC7E3
+	.INCBIN"bin/battle_bgs/graphics/97.bin"
+
+BATTLE_BACKGROUND_GFX_94: ;$CBC82A
+	.INCBIN"bin/battle_bgs/graphics/94.bin"
+
+BATTLE_BACKGROUND_GFX_55: ;$CBC870
+	.INCBIN"bin/battle_bgs/graphics/55.bin"
+
+BATTLE_BACKGROUND_GFX_92: ;$CBC8B6
+	.INCBIN"bin/battle_bgs/graphics/92.bin"
+
+BATTLE_BACKGROUND_GFX_96: ;$CBC8FB
+	.INCBIN"bin/battle_bgs/graphics/96.bin"
+
+BATTLE_BACKGROUND_GFX_69: ;$CBC93A
+	.INCBIN"bin/battle_bgs/graphics/69.bin"
+
+BATTLE_BACKGROUND_GFX_13: ;$CBC978
+	.INCBIN"bin/battle_bgs/graphics/13.bin"
+
+BATTLE_BACKGROUND_GFX_35: ;$CBC9B6
+	.INCBIN"bin/battle_bgs/graphics/35.bin"
+
+BATTLE_BACKGROUND_GFX_95: ;$CBC9F3
+	.INCBIN"bin/battle_bgs/graphics/95.bin"
+
+BATTLE_BACKGROUND_GFX_26: ;$CBCA2D
+	.INCBIN"bin/battle_bgs/graphics/26.bin"
+
+BATTLE_BACKGROUND_GFX_28: ;$CBCA67
+	.INCBIN"bin/battle_bgs/graphics/28.bin"
+
+BATTLE_BACKGROUND_GFX_33: ;$CBCA9E
+	.INCBIN"bin/battle_bgs/graphics/33.bin"
+
+BATTLE_BACKGROUND_GFX_78: ;$CBCAD4
+	.INCBIN"bin/battle_bgs/graphics/78.bin"
+
+BATTLE_BACKGROUND_GFX_11: ;$CBCB04
+	.INCBIN"bin/battle_bgs/graphics/11.bin"
+
+BATTLE_BACKGROUND_GFX_45: ;$CBCB34
+	.INCBIN"bin/battle_bgs/graphics/45.bin"
+
+BATTLE_BACKGROUND_GFX_58: ;$CBCB5E
+	.INCBIN"bin/battle_bgs/graphics/58.bin"
+
+BATTLE_BACKGROUND_GFX_46: ;$CBCB87
+	.INCBIN"bin/battle_bgs/graphics/46.bin"
+
+BATTLE_BACKGROUND_GFX_57: ;$CBCBAF
+	.INCBIN"bin/battle_bgs/graphics/57.bin"
+
+BATTLE_BACKGROUND_GFX_29: ;$CBCBD2
+	.INCBIN"bin/battle_bgs/graphics/29.bin"
+
+BATTLE_BACKGROUND_GFX_48: ;$CBD6B4
+	.INCBIN"bin/battle_bgs/graphics/48.bin"
+
+BATTLE_BACKGROUND_GFX_50: ;$CBD6D2
+	.INCBIN"bin/battle_bgs/graphics/50.bin"
+
+BATTLE_BACKGROUND_GFX_44: ;$CBD6ED
+	.INCBIN"bin/battle_bgs/graphics/44.bin"
+
+BATTLE_BACKGROUND_GFX_30: ;$CBD705
+	.INCBIN"bin/battle_bgs/graphics/30.bin"
+
+BATTLE_BACKGROUND_GFX_12: ;$CBD71C
+	.INCBIN"bin/battle_bgs/graphics/12.bin"
+
+BATTLE_BACKGROUND_GFX_49: ;$CBD72E
+	.INCBIN"bin/battle_bgs/graphics/49.bin"
+
+BATTLE_BACKGROUND_GFX_67: ;$CBD73D
+	.INCBIN"bin/battle_bgs/graphics/67.bin"
+
+BATTLE_BACKGROUND_GFX_73: ;$CBD82A
+	.INCBIN"bin/battle_bgs/graphics/73.bin"
+
+BATTLE_BACKGROUND_ARR_90: ;$CBD830
+	.INCBIN"bin/battle_bgs/arrangements/90.bin"
+
+BATTLE_BACKGROUND_ARR_87: ;$CBD835
+	.INCBIN"bin/battle_bgs/arrangements/87.bin"
+
+BATTLE_BACKGROUND_ARR_83: ;$CBD83A
+	.INCBIN"bin/battle_bgs/arrangements/83.bin"
+
+BATTLE_BACKGROUND_ARR_81: ;$CBD83F
+	.INCBIN"bin/battle_bgs/arrangements/81.bin"
+
+BATTLE_BACKGROUND_ARR_80: ;$CBD844
+	.INCBIN"bin/battle_bgs/arrangements/80.bin"
+
+BATTLE_BACKGROUND_ARR_79: ;$CBD849
+	.INCBIN"bin/battle_bgs/arrangements/79.bin"
+
+BATTLE_BACKGROUND_ARR_77: ;$CBD84E
+	.INCBIN"bin/battle_bgs/arrangements/77.bin"
+
+BATTLE_BACKGROUND_ARR_76: ;$CBD853
+	.INCBIN"bin/battle_bgs/arrangements/76.bin"
+
+BATTLE_BACKGROUND_ARR_75: ;$CBD858
+	.INCBIN"bin/battle_bgs/arrangements/75.bin"
+
+BATTLE_BACKGROUND_ARR_74: ;$CBD85D
+	.INCBIN"bin/battle_bgs/arrangements/74.bin"
+
+BATTLE_BACKGROUND_ARR_73: ;$CBD862
+	.INCBIN"bin/battle_bgs/arrangements/73.bin"
+
+BATTLE_BACKGROUND_ARR_72: ;$CBD867
+	.INCBIN"bin/battle_bgs/arrangements/72.bin"
+
+BATTLE_BACKGROUND_ARR_71: ;$CBD86C
+	.INCBIN"bin/battle_bgs/arrangements/71.bin"
+
+BATTLE_BACKGROUND_ARR_0: ;$CBD871
+	.INCBIN"bin/battle_bgs/arrangements/0.bin"
+
+BATTLE_BACKGROUND_GFX_87: ;$CBD876
+	.INCBIN"bin/battle_bgs/graphics/87.bin"
+
+BATTLE_BACKGROUND_GFX_83: ;$CBD879
+	.INCBIN"bin/battle_bgs/graphics/83.bin"
+
+BATTLE_BACKGROUND_GFX_81: ;$CBD87C
+	.INCBIN"bin/battle_bgs/graphics/81.bin"
+
+BATTLE_BACKGROUND_GFX_80: ;$CBD87F
+	.INCBIN"bin/battle_bgs/graphics/80.bin"
+
+BATTLE_BACKGROUND_GFX_79: ;$CBD882
+	.INCBIN"bin/battle_bgs/graphics/79.bin"
+
+BATTLE_BACKGROUND_GFX_77: ;$CBD885
+	.INCBIN"bin/battle_bgs/graphics/77.bin"
+
+BATTLE_BACKGROUND_GFX_76: ;$CBD888
+	.INCBIN"bin/battle_bgs/graphics/76.bin"
+
+BATTLE_BACKGROUND_GFX_75: ;$CBD88B
+	.INCBIN"bin/battle_bgs/graphics/75.bin"
+
+BATTLE_BACKGROUND_GFX_74: ;$CBD88E
+	.INCBIN"bin/battle_bgs/graphics/74.bin"
+
+BATTLE_BACKGROUND_GFX_72: ;$CBD891
+	.INCBIN"bin/battle_bgs/graphics/72.bin"
+
+BATTLE_BACKGROUND_GFX_71: ;$CBD894
+	.INCBIN"bin/battle_bgs/graphics/71.bin"
+
+BATTLE_BACKGROUND_GFX_0: ;$CBD897
+	.INCBIN"bin/battle_bgs/graphics/0.bin"
 
 BTL_ENTRY_BG_TABLE: ;$CBD89A
 	.INCBIN "bin/unknowns/CBD89A.bin"
