0d7d2b7654816956f50bd2f492c97a6aed9e6d88
Cameron Ross <elpenguino@gmail.com>
2018-10-22 20:05:56 -0300

    continue the splitting

---
+++ src/data/sprite_group_palettes.asm
@@ -0,0 +1,10 @@
+
+SPRITE_GROUP_PALETTES: ;$C30000
+	.INCBIN "bin/overworld_sprites/0.pal"
+	.INCBIN "bin/overworld_sprites/1.pal"
+	.INCBIN "bin/overworld_sprites/2.pal"
+	.INCBIN "bin/overworld_sprites/3.pal"
+	.INCBIN "bin/overworld_sprites/4.pal"
+	.INCBIN "bin/overworld_sprites/5.pal"
+	.INCBIN "bin/overworld_sprites/6.pal"
+	.INCBIN "bin/overworld_sprites/7.pal"
