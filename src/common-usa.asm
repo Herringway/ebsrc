eb7bfc7e46ee65702edaa49bef38638f38d07e7f
Cameron Ross <elpenguino@gmail.com>
2020-11-11 18:28:44 -0400

    add some symbols

--- src/common-usa.asm
+++ src/common-usa.asm
@@ -1,6 +1,6 @@
+.DEFINE LOCALE "US"
 .INCLUDE "macros.asm"
 .INCLUDE "hardware.asm"
 .INCLUDE "enums.asm"
-.DEFINE LOCALE "US"
 .INCLUDE "symbols-usa.asm"
 .SMART
--- src/common-usa19950327.asm
+++ src/common-usa19950327.asm
@@ -1,6 +1,6 @@
+.DEFINE LOCALE "US19950327"
 .INCLUDE "macros.asm"
 .INCLUDE "hardware.asm"
 .INCLUDE "enums.asm"
-.DEFINE LOCALE "US19950327"
 .INCLUDE "symbols-usa19950327.asm"
 .SMART
