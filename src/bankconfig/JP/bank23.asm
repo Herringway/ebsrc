b46abd77c85e63205b7cd8d9625ad2a2364f7f76
Cooper Harasyn <charasyn@outlook.com>
2021-09-27 23:38:29 -0700

    Modify macros.asm to build on latest ca65
    As of May 2nd, 2021, .INCLUDE behaviour inside of macros was modified in
    ca65. Now, it requires that the file to be included is known at macro
    definition time, and substitutes it then. This breaks the AUDIOPACK
    macro, so we work around it by removing it. This is OK since AUDIOPACK
    is only used for the pack terminators - we add a new macro just for this
    purpose: AUDIOPACKTERMINATOR
    
    The guilty change to ca65: https://github.com/cc65/cc65/commit/b9a3c7888822732a0de92741cfe1a3e1b6bb272f
    
    Further discussion: https://github.com/cc65/cc65/issues/1503
    
    Fixes: https://github.com/Herringway/ebsrc/issues/14

--- src/bankconfig/JP/bank23.asm
+++ src/bankconfig/JP/bank23.asm
@@ -5,20 +5,20 @@ AUDIO_PACK_3: ;$E30000
         AUDIOPACKBIN $24, $6C2C, "audiopacks/3-1.bin"
         AUDIOPACKBIN $5EFC, $87C4, "audiopacks/3-2.bin"
         AUDIOPACKBIN $36, $6E42, "audiopacks/3-3.bin"
-        AUDIOPACK 0
+        AUDIOPACKTERMINATOR
 
 AUDIO_PACK_70: ;$E35F64
         AUDIOPACKBIN $28, $6C68, "audiopacks/70-1.bin"
         AUDIOPACKBIN $5130, $95B0, "audiopacks/70-2.bin"
         AUDIOPACKBIN $30, $6E9C, "audiopacks/70-3.bin"
-        AUDIOPACK 0
+        AUDIOPACKTERMINATOR
 
 AUDIO_PACK_37: ;$E3B0FA
         AUDIOPACKBIN $28, $6C68, "audiopacks/37-1.bin"
         AUDIOPACKBIN $4C60, $95B0, "audiopacks/37-2.bin"
         AUDIOPACKBIN $3C, $6E9C, "audiopacks/37-3.bin"
-        AUDIOPACK 0
+        AUDIOPACKTERMINATOR
 
 AUDIO_PACK_32: ;$E3FDCC
         AUDIOPACKBIN $220, $5800, "audiopacks/32.bin"
-        AUDIOPACK 0
+        AUDIOPACKTERMINATOR
