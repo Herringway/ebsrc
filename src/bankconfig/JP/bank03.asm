51424aca282702d02b330903a1528dc321a0f2a4
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2024-02-03 09:35:54 -0600

    Identify final antipiracy check

--- src/bankconfig/JP/bank03.asm
+++ src/bankconfig/JP/bank03.asm
@@ -2144,6 +2144,6 @@ SPRITE_GROUP_PALETTES: ;$C30000
 
 .INCLUDE "data/unused_for_sale_sign_sprite_table.asm"
 
-.INCLUDE "unknown/C3/C3FDC5.asm"
+.INCLUDE "system/antipiracy/final_antipiracy_check.asm"
 
 .INCLUDE "data/antipiracy_checksum_2.asm"
